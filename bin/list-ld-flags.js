cdd0896891378ffc24f53070f5fcb83fe7d23f4b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-09-17 18:31:25 -0400

    fix: compare flag to a flag

--- bin/list-ld-flags.js
+++ bin/list-ld-flags.js
@@ -69,7 +69,7 @@ ldApi
           console.log(`Flag ${flag.key} was removed`)
           return
         }
-        const str = diffString(previous, flag, { color: true })
+        const str = diffString(current, flag, { color: true })
         if (str) {
           differences.push({
             key: flag.key,
